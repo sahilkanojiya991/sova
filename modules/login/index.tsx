de383e255af6783ee296a114d57eba2158a417f1
sahilkanojiya991 <141848117+sahilkanojiya991@users.noreply.github.com>
2023-08-17 19:42:43 +0530

    second commit

--- modules/login/index.tsx
+++ modules/login/index.tsx
@@ -1,84 +1,240 @@
+
 import type { NextPage } from "next";
-import styles from "./modules/index.module.css";
+import styles from "../index.module.css"
 import { Image } from "react-bootstrap";
 
 const Login: NextPage = () => {
+
   return (
-    <div className={styles.signUpPage}>
-      <div className={styles.dObjects}>
-        <div className={styles.dObjectsChild} />
-        <div
-          className={styles.find3dObjects}
-        >{`Find 3D Objects , Mockups and illustration here `}</div>
-        <Image className={styles.vectorIcon} alt="" src="/vector.svg" />
-        <Image
-          className={styles.abstractionIcon}
-          alt=""
-          src="/abstraction@2x.png"
-        />
-        <Image
-          className={styles.abstractionIcon1}
-          alt=""
-          src="/abstraction1@2x.png"
-        />
-        <Image
-          className={styles.abstractionIcon2}
-          alt=""
-          src="/abstraction2@2x.png"
-        />
-      </div>
-      <div className={styles.language}>
-        <div className={styles.englishUk}>{`English ( UK ) `}</div>
-        <Image className={styles.languageChild} alt="" src="/polygon-1.svg" />
-      </div>
-      <div className={styles.loginDetails}>
-        <div className={styles.or}>-OR-</div>
-        <div className={styles.fullName}>{`Full Name `}</div>
-        <div className={styles.eMailAddress}>{`E-mail Address `}</div>
-        <Image className={styles.loginDetailsChild} alt="" src="/line-1.svg" />
-        <Image className={styles.loginDetailsItem} alt="" src="/line-1.svg" />
-        <Image className={styles.loginDetailsInner} alt="" src="/line-1.svg" />
-        <div className={styles.frame}>
-          <div className={styles.password}>{`Password `}</div>
-          <Image className={styles.blind1Icon} alt="" src="/blind-1.svg" />
-        </div>
-      </div>
-      <div className={styles.createAccount}>
-        <b className={styles.createAccount1}>{`Create Account `}</b>
-        <div className={styles.frame1}>
-          <div className={styles.frame2}>
-            <Image
-              className={styles.googleLogoIconPng}
-              alt=""
-              src="/googlelogoiconpngtransparentbackgroundosteopathy16-1@2x.png"
-            />
-            <div className={styles.signUpWith}>Sign up with Google</div>
+    <div className={styles.login}>
+      <div className={styles.newHeader}>
+        <Image className={styles.noiseIcon} alt="" src="/noise@2x.png" />
+        <div className={styles.parent}>
+          <Image className={styles.icon} alt="" src="/-2627267886144.svg" />
+          <div className={styles.frameParent}>
+            <div className={styles.frameGroup}>
+              <div className={styles.linkParent}>
+                <div className={styles.link}>
+                  <div className={styles.masterLink}>
+                    <Image
+                      className={styles.lineRoundedpasswordIcon}
+                      alt=""
+                      src="/line-roundedpassword.svg"
+                    />
+                    <div className={styles.linkItem}>Individual</div>
+                    <Image
+                      className={styles.lineRoundedpasswordIcon}
+                      alt=""
+                      src="/line-roundedchevron-down.svg"
+                    />
+                  </div>
+                </div>
+                <Image
+                  className={styles.frameChild}
+                  alt=""
+                  src="/vector-337.svg"
+                />
+                <div className={styles.link}>
+                  <div className={styles.masterLink}>
+                    <Image
+                      className={styles.lineRoundedpasswordIcon}
+                      alt=""
+                      src="/line-roundedpassword1.svg"
+                    />
+                    <div className={styles.linkItem}>Business</div>
+                    <Image
+                      className={styles.lineRoundedpasswordIcon}
+                      alt=""
+                      src="/line-roundedchevron-down1.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.linkGroup}>
+                <div className={styles.link}>
+                  <div className={styles.masterLink}>
+                    <Image
+                      className={styles.lineRoundedpasswordIcon}
+                      alt=""
+                      src="/line-roundedpassword2.svg"
+                    />
+                    <div className={styles.linkItem}>Cards</div>
+                    <Image
+                      className={styles.lineRoundedpasswordIcon}
+                      alt=""
+                      src="/line-roundedchevron-down2.svg"
+                    />
+                  </div>
+                </div>
+                <div className={styles.link}>
+                  <div className={styles.masterLink}>
+                    <Image
+                      className={styles.lineRoundedpasswordIcon}
+                      alt=""
+                      src="/line-roundedpassword2.svg"
+                    />
+                    <div className={styles.linkItem}>Fees</div>
+                    <Image
+                      className={styles.lineRoundedpasswordIcon}
+                      alt=""
+                      src="/line-roundedchevron-down1.svg"
+                    />
+                  </div>
+                </div>
+                <div className={styles.link}>
+                  <div className={styles.masterLink}>
+                    <Image
+                      className={styles.lineRoundedpasswordIcon}
+                      alt=""
+                      src="/line-roundedpassword2.svg"
+                    />
+                    <div className={styles.linkItem}>Developer</div>
+                    <Image
+                      className={styles.lineRoundedpasswordIcon}
+                      alt=""
+                      src="/line-roundedchevron-down3.svg"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.frameContainer}>
+              <div className={styles.linkGroup}>
+                <div className={styles.masterLink}>
+                  <Image
+                    className={styles.unitedKingdomIcon}
+                    alt=""
+                    src="/united-kingdom.svg"
+                  />
+                  <div className={styles.enWrapper}>
+                    <div className={styles.en}>EN</div>
+                  </div>
+                </div>
+                <div className={styles.login1}>
+                  <Image
+                    className={styles.clarityloginLineIcon}
+                    alt=""
+                    src="/clarityloginline.svg"
+                  />
+                  <div className={styles.buttonText}>Login</div>
+                </div>
+              </div>
+              <div className={styles.button}>
+                <div className={styles.getApp}>Get App</div>
+              </div>
+            </div>
           </div>
-          <div className={styles.frame3}>
-            <Image
-              className={styles.facebook5121Icon}
-              alt=""
-              src="/facebook512-1@2x.png"
-            />
-            <div className={styles.signUpWith}>Sign up with Facebook</div>
-          </div>
+        </div>
+        <div className={styles.newHeaderChild} />
+      </div>
+      <div className={styles.text}>
+        <div className={styles.loginToYour}>Login to Your Account</div>
+        <div className={styles.voremIpsumDolorContainer}>
+          <p
+            className={styles.voremIpsumDolor}
+          >{`Vorem ipsum dolor sit amet, consectetur adipiscing elit. Vorem ipsum dolor `}</p>
+          <p
+            className={styles.voremIpsumDolor}
+          >{`sit amet, consectetur adipiscing elit.  `}</p>
         </div>
       </div>
-      <div className={styles.button}>
-        <div className={styles.frame4}>
-          <b className={styles.signUpWith}>Create Account</b>
+      <div className={styles.forgotPassword}>Forgot Password?</div>
+      <div className={styles.links}>
+        <div className={styles.privacyPolicy}>Privacy Policy</div>
+        <div className={styles.privacyPolicy}>Copyright@aaaa 2023</div>
+      </div>
+      <div className={styles.frame}>
+        <Image className={styles.frameIcon} alt="" src="/frame.svg" />
+        <div className={styles.fields}>
+          <div className={styles.textFieldDark}>
+            <div className={styles.textField}>
+              <div className={styles.stateLayer}>
+                <div className={styles.content}>
+                  <div className={styles.inputText}>
+                    <div className={styles.inputText1}>
+                      Email / Phone number
+                    </div>
+                  </div>
+                  <div className={styles.labelText}>
+                    <div className={styles.labelText1}>Label</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.supportingText}>
+              <div className={styles.supportingText1}>Supporting text</div>
+            </div>
+          </div>
+          <div className={styles.textFieldDark1}>
+            <div className={styles.textField1}>
+              <div className={styles.stateLayer}>
+                <div className={styles.content}>
+                  <div className={styles.inputText}>
+                    <div className={styles.inputText1}>6 Digit Pin</div>
+                  </div>
+                  <div className={styles.labelText}>
+                    <div className={styles.labelText1}>Label</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.supportingText}>
+              <div className={styles.supportingText1}>Supporting text</div>
+            </div>
+          </div>
+          <div className={styles.button1}>
+            <div className={styles.button2}>
+              <div className={styles.stateLayer2}>
+                <b className={styles.labelText4}>Login to Your Account</b>
+              </div>
+              <Image
+                className={styles.buttonChild}
+                alt=""
+                src="/group-1000002648.svg"
+              />
+            </div>
+            <div className={styles.supportingText4}>
+              <div className={styles.supportingText5}>
+                <span
+                  className={styles.dontHaveAn}
+                >{`Don’t have an account yet? `}</span>
+                <span className={styles.registerNow}>Register now!</span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.alreadyHaveAnContainer}>
-          <span>Already have an account ?</span>
-          <b className={styles.logIn}>
-            <span className={styles.span}>{` `}</span>
-            <span>Log in</span>
-          </b>
-          <span className={styles.span1}>{` `}</span>
+        <div className={styles.socials}>
+          <div className={styles.button3}>
+            <Image
+              className={styles.ionlogoGoogleIcon}
+              alt=""
+              src="/ionlogogoogle.svg"
+            />
+            <div className={styles.stateLayer2}>
+              <div className={styles.inputText1}>Sign in with Google</div>
+            </div>
+          </div>
+          <div className={styles.button3}>
+            <Image className={styles.vectorIcon} alt="" src="/vector.svg" />
+            <div className={styles.stateLayer4}>
+              <div className={styles.inputText1}>Sign in with Facebook</div>
+            </div>
+          </div>
+          <div className={styles.button3}>
+            <Image className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+            <div className={styles.stateLayer2}>
+              <div className={styles.inputText1}>
+                Sign in with Apple Account
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+
   );
 };
 
 export default Login;
+
+9923954798
